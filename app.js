29891e7b68d374a09bdc2f3e1dbf782b2ce20c3d
aliffrhn <alif.farhanw@gmail.com>
2019-07-11 10:52:29 +0700

    serve iteration datas

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ var b = [0, 3, 9, 7, 0, 0];
 var c = [0, 0, 7, 11, 7, 0];
 var d = [0, 0, 0, 13, 3, 9];
 var e = [5, 0, 0, 0, 5, 5];
+
+var jk = 5;
+var jp = 6;
+var iteration = 0;
+var maxiteration = 100000;
+
+var temp = 1000;
+var alpha = 0.995;
